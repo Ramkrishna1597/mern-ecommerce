5a2af40f41f0499f02f07858ce73d7f63e3d3929
Ramkrishna1597 <97449969+Ramkrishna1597@users.noreply.github.com>
2022-09-05 15:26:07 +0530

    navbar-create

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="http:/" rel="noopener noreferrer">myzone</a>
+      </header>
+      <main>lst product</main>
+    </div>
+  );
+}
+
+export default App;
