4c919da60beecbaab688ecbbfb2733f4803ee0f8
Ramkrishna1597 <97449969+Ramkrishna1597@users.noreply.github.com>
2022-09-06 22:29:01 +0530

    products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -6,7 +7,29 @@ function App() {
       <header className="App-header">
         <a href="http:/" rel="noopener noreferrer">myzone</a>
       </header>
-      <main>lst product</main>
+      <main>
+        <h1> Featured Products</h1>
+        <div className="products">
+        {
+          data.products.map(product => (
+          <div className="product" key={product.price}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name}/>
+            </a>
+            <div className="product-info">
+              <p>
+                {product.name}
+              </p>
+              <p>
+                {product.price}
+              </p>
+              <button>Add to Cart</button>
+            </div>
+          </div>))
+        }
+        </div>
+
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products:[
+        {
+            name:"Nike Slim shirt",
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/1.jpg', //679 x 829
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt'
+        },
+        {
+            name:"Pants",
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/2.jpg',
+            price:800,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality pants'
+        },
+        {
+            name:"Nike t-shirt",
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/3.jpg',
+            price:780,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality t-shirt'
+        },
+        {
+            name:"Nike t-shirt",
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/4.jpg',
+            price:680,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality t-shirt'
+        },
+    ]
+}
+
+export default data
